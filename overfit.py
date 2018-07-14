d064e3fd9ab40493916b37ff77ea4bd84a6ce9aa
iamlucaswolf <iamlucaswolf@gmail.com>
2018-07-14 16:21:37 +0200

    Add gradient checkpointing

--- overfit.py
+++ overfit.py
@@ -6,12 +6,17 @@ from data.transforms import AffineWarp, Elongate
 from yolophem import models
 from caps import utils
 
+from gradient_checkpointing import memory_saving_gradients
+
+# Monkey-patch tf.gradients to memory_saving_gradients
+tf.__dict__["gradients"] = memory_saving_gradients.gradients_memory
+
 ## SETTINGS
 
 OVERFIT_SAMPLES = 1
 MODEL = models.naive
-CONFIG = models.config_v1
-PARAMS = { 'feature_size': 4096 }
+CONFIG = models.config_v2
+PARAMS = { 'feature_size': 512 }
 STEP_SIZE = 1e-3
 NUM_EPOCHS = 1000
 
@@ -25,7 +30,7 @@ if __name__ == '__main__':
 
     # Build the model
     reset()
-    [X, y], _, loss = MODEL(CONFIG, **PARAMS)
+    [X, y], [predictions, confidences], loss = MODEL(CONFIG, **PARAMS)
 
     feed_dict = {
         X: images,
@@ -40,14 +45,19 @@ if __name__ == '__main__':
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
+        print('Start session')
         init.run()
+        print('Initialized')
 
-        for epoch in range(NUM_EPOCHS):
+        loss_val = sess.run(loss, feed_dict=feed_dict)
+        print(loss_val)
+
+         #for epoch in range(NUM_EPOCHS):
 
             # Run a train step           
-            sess.run(train_step, feed_dict=feed_dict)
+            #sess.run(train_step, feed_dict=feed_dict)
 
             # Evaluate loss
-            epoch_loss = sess.run([loss], feed_dict=feed_dict)
+            #epoch_loss = sess.run([loss], feed_dict=feed_dict)
 
-            print('[{}/{}] Loss: {}'.format(epoch+1, NUM_EPOCHS, epoch_loss))
+            #print('[{}/{}] Loss: {}'.format(epoch+1, NUM_EPOCHS, epoch_loss))
